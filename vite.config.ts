c5bc335eb62d972d486d156ac28ac6547c1291c8
Juan Rodriguez <juan@juanrod.dev>
2025-03-12 17:47:42 -0500

    deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/futoro-site/',  // Add this line - replace <REPO-NAME> with your repository name
   plugins: [vue()],
   resolve: {
     alias: {
